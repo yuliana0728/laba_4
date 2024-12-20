aeec834d6e71ca297e01f996ae6a39a8f5e9c0d6
yuliana0728 <wenxty@icloud.com>
2024-12-20 09:48:53 +0300

    Задание 14.py

---
+++ Задание 14.py
@@ -0,0 +1,11 @@
+class Employee:
+  def __init__(self,name, salary):
+    self.name = name
+    self.salary = salary
+
+    def getSalary(self, __cape):
+		self.__cape (self.addSign(self.salary))
+
+
+	def addSign(self,num):
+		return num + '$'
