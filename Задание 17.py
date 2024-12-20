e13c872e31d2cbac69ec7c4af110176e1eb43532
yuliana0728 <wenxty@icloud.com>
2024-12-20 10:04:36 +0300

    Задание 17.py

---
+++ Задание 17.py
@@ -0,0 +1,17 @@
+class Employee:
+  def __init__(self,name, salary, age):
+      self.__name = name
+      self.__salary = salary
+      salary.__age = age
+ def getName(self):
+    return self.name
+ def getSary(self):
+    return self.salary
+ def getAge(self):
+    return self.age
+ def setName(self, name):
+              self.__name = name
+ def setSalary(self, salary):
+              self.__salary = salary
+ def setAge(self, age):
+              self.__age = age
