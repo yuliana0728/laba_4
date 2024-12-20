c56772e9ce7448f451c893e4124f5268626d530e
yuliana0728 <wenxty@icloud.com>
2024-12-20 09:57:02 +0300

    Задание 16.py

---
+++ Задание 16.py
@@ -0,0 +1,11 @@
+class Employee:
+  def __init__(self,name, salary, age):
+      self.__name = name
+      self.__salary = salary
+      salary.__age = age
+ def getName(self):
+    return self.name
+ def getSary(self):
+    return self.salary
+ def getAge(self):
+    return self.age
